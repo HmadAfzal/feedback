f8c789699f97f3170b62ab8108c4debcebda0ccd
Hmad Afzal <hmadafzal00@gmail.com>
2024-08-04 16:51:08 +0500

    env.example added

--- src/components/dashboard/FormPreview.tsx
+++ src/components/dashboard/FormPreview.tsx
@@ -30,7 +30,7 @@ const FormPreview = ({ formData, selectedFile, setCreateSpace, user, imageFile, 
             if (selectedFile) {
                 const formData = new FormData();
                 formData.append('file', selectedFile);
-                formData.append('upload_preset', 'testimonial');
+                formData.append('upload_preset', 'testimonial');  // create an upload preset and enter it here
 
                 const response = await fetch(`https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload`, {
                     method: 'POST',
