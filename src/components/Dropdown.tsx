aee3bee9128b140a60b310ef6251674fa45a059b
Hmad Afzal <hmadafzal00@gmail.com>
2024-08-31 16:24:59 +0500

    dropdown improved

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,103 +1,96 @@
 import {
-    Computer,
-    Github,
-    LifeBuoy,
-    LogOut,
-    Menu,
-    Moon,
-    Settings,
-    Sun,
-    User,
-  } from "lucide-react"
-  
-  import { Button } from "@/components/ui/button"
-  import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuGroup,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuPortal,
-    DropdownMenuSeparator,
-    DropdownMenuShortcut,
-    DropdownMenuSub,
-    DropdownMenuSubContent,
-    DropdownMenuSubTrigger,
-    DropdownMenuTrigger,
-  } from "@/components/ui/dropdown-menu"
-import { useTheme } from "next-themes"
+  Computer,
+  ComputerIcon,
+  Github,
+  LifeBuoy,
+  LogOut,
+  Menu,
+  Moon,
+  MoonIcon,
+  PlusCircle,
+  Sun,
+  SunIcon,
+} from "lucide-react";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useTheme } from "next-themes";
 import { clearSpaceSlice } from "@/redux/spaceslice";
 import { signOut } from "next-auth/react";
 import { useAppDispatch } from "@/redux/hooks";
-  
-  export function Dropdown() {
-    const { setTheme } = useTheme();
-    const dispatch=useAppDispatch()
-    const handleSignout = async () => {
-        dispatch(clearSpaceSlice())
-        await signOut();
-      
-   };
-    return (
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="ghost"><Menu/></Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="w-64 border-neutral-800 p-3 rounded-lg dark:bg-[#121212] bg-[#ffffff]">
-          <DropdownMenuGroup>
-            <DropdownMenuItem>
-              <User className="mr-2 h-4 w-4" />
-              <span>Account  setting</span>
-             
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              <Settings className="mr-2 h-4 w-4" />
-              <span>Dashboard</span>
-            </DropdownMenuItem>
-          </DropdownMenuGroup>
-          <DropdownMenuSeparator />
-          <DropdownMenuGroup>
-            <DropdownMenuSub>
-              <DropdownMenuSubTrigger>
-                <Sun className="mr-2 h-4 w-4" />
-                <span>Theme</span>
-              </DropdownMenuSubTrigger>
-              <DropdownMenuPortal>
-                <DropdownMenuSubContent className="border-neutral-800 p-2 rounded-lg dark:bg-[#121212] bg-[#ffffff]">
-                  <DropdownMenuItem>
-                    <Sun className="mr-2 h-4 w-4" />
-                    <span onClick={() => setTheme("light")}>Light</span>
-                  </DropdownMenuItem>
-                  <DropdownMenuItem>
-                    <Moon className="mr-2 h-4 w-4" />
-                    <span onClick={() => setTheme("dark")}>Dark</span>
-                  </DropdownMenuItem>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuItem>
-                    <Computer className="mr-2 h-4 w-4" />
-                    <span onClick={() => setTheme("system")}>System</span>
-                  </DropdownMenuItem>
-                </DropdownMenuSubContent>
-              </DropdownMenuPortal>
-            </DropdownMenuSub>
-          </DropdownMenuGroup>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>
-            <Github className="mr-2 h-4 w-4" />
-            <span>GitHub</span>
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+export function Dropdown() {
+  const { theme, setTheme } = useTheme();
+  const dispatch = useAppDispatch();
+  const handleSignout = async () => {
+    dispatch(clearSpaceSlice());
+    await signOut();
+  };
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost">
+          <Menu />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-64 dark:border-neutral-800 border-neutral-300 p-3 rounded-lg dark:bg-[#121212] bg-[#ffffff] text-muted-foreground">
+        <DropdownMenuGroup>
+          <DropdownMenuItem className="flex items-center justify-between py-2 rounded-lg px-2">
+            <span>Dashboard</span>
           </DropdownMenuItem>
-          <DropdownMenuItem>
-            <LifeBuoy className="mr-2 h-4 w-4" />
+          <DropdownMenuItem className="flex items-center justify-between py-2 rounded-lg px-2">
+            <span>
+              <Link href={"/create-space"}>Create space</Link>
+            </span>
+            <PlusCircle className="mr-2 h-4 w-4" />
+          </DropdownMenuItem>
+          <DropdownMenuItem className="flex items-center justify-between rounded-lg px-2 hover:bg-transparent">
+            <span>Theme</span>
+            <Tabs defaultValue={theme}>
+              <TabsList className="flex rounded-full bg-neutral-200 dark:bg-neutral-900">
+                <TabsTrigger value="system" className="p-2 rounded-full " onClick={() => setTheme("system")} >
+                  <ComputerIcon className="size-3" />
+                </TabsTrigger>
+                <TabsTrigger value="light" className="p-2 rounded-full " onClick={() => setTheme("light")} >
+                  <SunIcon className="size-3" />
+                </TabsTrigger>
+                <TabsTrigger value="dark" className="p-2 rounded-full " onClick={() => setTheme("dark")} >
+                  <MoonIcon className="size-3" />
+                </TabsTrigger>
+              </TabsList>
+            </Tabs>
+          </DropdownMenuItem>
+
+          <DropdownMenuSeparator />
+          <DropdownMenuItem className="flex items-center justify-between py-2 rounded-lg px-2">
+            <span><Link href={"https://github.com/HmadAfzal/feedback"}>GitHub</Link></span>
+          </DropdownMenuItem>
+          <DropdownMenuItem className="flex items-center justify-between py-2 rounded-lg px-2">
             <span>Support</span>
+            <LifeBuoy className="mr-2 h-4 w-4" />
           </DropdownMenuItem>
           <DropdownMenuSeparator />
-          <DropdownMenuItem onClick={handleSignout}>
-            <LogOut className="mr-2 h-4 w-4" />
+          <DropdownMenuItem
+            onClick={handleSignout}
+            className="flex items-center justify-between py-2 rounded-lg px-2"
+          >
             <span>Log out</span>
-            <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+            <LogOut className="mr-2 h-4 w-4" />
           </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    )
-  }
-  
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,21 +1,11 @@
 'use client'
 import React from 'react'
-import { Button } from '@/components/ui/button'
-import { ModeToggle } from '@/components/ui/Modetoggle'
-import { signOut } from 'next-auth/react'
-import { useAppDispatch } from '@/redux/hooks'
-import { clearSpaceSlice } from '@/redux/spaceslice'
 import Link from 'next/link'
 import { Dropdown } from './Dropdown'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+
 
 const Nav = () => {
-    const dispatch = useAppDispatch();
-
-    const handleSignout = async () => {
-         dispatch(clearSpaceSlice())
-         await signOut();
-       
-    };
     return (
         <nav className='flex items-center justify-between py-6'>
             <h1 className='font-bold text-3xl'><Link href={'/'}>Feedback</Link></h1>
