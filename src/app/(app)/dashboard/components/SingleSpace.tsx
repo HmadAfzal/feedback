cea6dd39c248d78000ee3eb62266afc9175e7bd4
Hmad Afzal <hmadafzal00@gmail.com>
2024-08-31 16:32:29 +0500

    space styling improved

--- src/app/(app)/dashboard/components/SingleSpace.tsx
+++ src/app/(app)/dashboard/components/SingleSpace.tsx
@@ -9,10 +9,10 @@ import SpaceSettings from '@/components/SpaceSettings';
 const SingleSpace = ({space}:{space: Space }) => {
     const baseUrl = `${window.location.protocol}//${window.location.host}`;
     const spaceUrl = `${baseUrl}/u/${space?.name}`;
-    const truncatedUrl = spaceUrl.length > 25 ? `${spaceUrl.slice(0, 25)}...` : spaceUrl;
+    const truncatedUrl = spaceUrl.length > 35 ? `${spaceUrl.slice(0, 35)}...` : spaceUrl;
 
     return (
-        <div className={`dark:bg-neutral-800 bg-neutral-200 py-5 w-full rounded-lg md:px-6 px-4 my-6 flex justify-between`}>
+        <div className={`dark:bg-neutral-800 bg-neutral-200 md:py-5 py-4 w-full rounded-lg md:px-6 px-4 md:my-6 my-4 flex justify-between`}>
             <div className='flex items-center md:gap-6 gap-3'>
                 <Avatar className={'md:h-16 md:w-16 h-12 w-12'}>
                     <AvatarImage src={space?.image} />
@@ -20,7 +20,7 @@ const SingleSpace = ({space}:{space: Space }) => {
                 </Avatar>
                 <div>
                     <h3 className='font-bold md:text-xl text-lg md:pb-2 pb-1'>{space?.name}</h3>
-                    <div className='flex items-center gap-2 text-muted-foreground hover:underline [w-90%]'>
+                    <div className='flex items-center gap-2 text-muted-foreground hover:underline [w-98%]'>
                         <Link2 className='md:size-5 size-4' />
                         <span className='block sm:hidden text-xs text-muted-foreground tracking-wide'>{truncatedUrl}</span>
                         <span className='hidden sm:block text-md  text-muted-foreground tracking-wide'>{spaceUrl}</span>
